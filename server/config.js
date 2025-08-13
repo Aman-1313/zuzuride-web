d2c420b0ec55555f7282435a3817814a14eea8df
AMAN PREET SINGH <amansainigahonia@gmail.com>
2025-08-12 18:37:50 -0600

    Create config.js
    Firebase config

---
+++ server/config.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: "AIzaSyBts_gvH-uVk7m3-rTkrCLlxJ7Vb_L7jSk",
+  authDomain: "skiptheride.firebaseapp.com",
+  projectId: "skiptheride",
+  storageBucket: "skiptheride.firebasestorage.app",
+  messagingSenderId: "577076618336",
+  appId: "1:577076618336:web:84cce92e93522c91ae8f05"
+};
+
+const app = initializeApp(firebaseConfig);
